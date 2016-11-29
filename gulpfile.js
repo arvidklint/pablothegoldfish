34f68c8f67ed4a3bee7b2e790b24c25153240dfa
Erik Forsberg <erik.c.forsberg@gmail.com>
2016-11-29 10:41:38 +0100

    Devmiljö upsatt

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var elixir = require('laravel-elixir');
+require('laravel-elixir-vue');
+require('laravel-elixir-browsersync-official');
+
+elixir.config.assetsPath = 'src';
+elixir.config.publicPath = 'dist';
+
+elixir(function(mix) {
+    mix.sass('app.scss')
+        .webpack('app.js')
+        .browserSync({
+            files: ['dist/*.html', 'src/sass/*.scss', 'src/js/*.js'],
+            proxy: undefined,
+            server: {
+                baseDir: "dist"
+            }
+        });
+});
